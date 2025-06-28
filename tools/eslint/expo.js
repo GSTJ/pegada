f879530fc540d3f18d3bd72fb4feef2d54f52527
Gabriel Taveira <gabrielstaveira@gmail.com>
2025-06-28 13:04:26 -0300

    fix

--- tools/eslint/expo.js
+++ tools/eslint/expo.js
@@ -2,10 +2,10 @@ import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 import { includeIgnoreFile } from "@eslint/compat";
 // @ts-ignore - magic-eslint-config doesn't provide TypeScript declarations
-import expoConfig from "magic-eslint-config/expo";
+import reactNativeConfig from "magic-eslint-config/react-native";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const ignoreFile = includeIgnoreFile(join(__dirname, "../../.gitignore"));
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [ignoreFile, ...expoConfig];
+export default [ignoreFile, ...reactNativeConfig];
