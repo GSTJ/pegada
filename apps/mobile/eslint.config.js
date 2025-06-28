b2dd55af27bb8854a8210bd8e45db69a4ee292cd
Gabriel Taveira <gabrielstaveira@gmail.com>
2025-06-28 15:40:14 -0300

    fix

--- apps/mobile/eslint.config.js
+++ apps/mobile/eslint.config.js
@@ -15,16 +15,16 @@ export default [
   {
     files: ["**/*.ts", "**/*.tsx"],
     rules: {
-      "@typescript-eslint/no-extraneous-class": "off",
+      "@typescript-eslint/no-extraneous-class": "off", // That's good, keep it
       "@shopify/no-namespace-imports": "off",
-      "@shopify/jsx-no-hardcoded-content": "off",
+      "@shopify/jsx-no-hardcoded-content": "off", // That's good, keep it
       "@shopify/prefer-early-return": "off",
       "@typescript-eslint/no-unnecessary-condition": "off",
       "@typescript-eslint/prefer-nullish-coalescing": "off",
       "@typescript-eslint/no-confusing-void-expression": "off",
       "@typescript-eslint/no-non-null-assertion": "off",
       "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unsafe-enum-comparison": "off",
+      "@typescript-eslint/no-unsafe-enum-comparison": "off", // That's good, keep it
       "@typescript-eslint/no-deprecated": "off",
       "@typescript-eslint/restrict-plus-operands": "off",
       "@typescript-eslint/consistent-type-definitions": "off",
@@ -32,7 +32,7 @@ export default [
       "@typescript-eslint/no-misused-promises": "off",
       "react/no-unstable-nested-components": "off",
       "react-hooks/exhaustive-deps": "off",
-      "react-native/no-inline-styles": "off",
+      "react-native/no-inline-styles": "off", // That's fine, keep it
       "@typescript-eslint/non-nullable-type-assertion-style": "off",
       "@typescript-eslint/use-unknown-in-catch-callback-variable": "off",
       "@typescript-eslint/only-throw-error": "off",
@@ -47,7 +47,6 @@ export default [
       "import/namespace": "off",
       "import/default": "off",
       "import/no-named-as-default-member": "off",
-      "@typescript-eslint/prefer-regexp-exec": "off",
       "@typescript-eslint/ban-tslint-comment": "off",
       "no-restricted-syntax": "off",
       "no-else-return": "off",
