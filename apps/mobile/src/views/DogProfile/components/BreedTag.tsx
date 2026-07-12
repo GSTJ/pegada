78e09ba6cc5692b4da06c5b9c3a8b819a6ed7390
Gabriel Taveira <gabrielstaveira@gmail.com>
2026-07-12 20:24:39 -0300

    improve

--- apps/mobile/src/views/DogProfile/components/BreedTag.tsx
+++ apps/mobile/src/views/DogProfile/components/BreedTag.tsx
@@ -5,11 +5,11 @@ import styled from "styled-components/native";
 import { BreedSlug } from "@pegada/shared/i18n/i18n";
 import { Namespace } from "@pegada/shared/i18n/types/types";
 
-import Glassmorphism from "@/components/Glassmorphism";
 import { Text } from "@/components/Text";
 import { SwipeDog } from "@/store/reducers/dogs/swipe";
+import { TransparentGlassOrDarkBlurView } from "@/components/BlurView";
 
-const GlassmorphismStyled = styled(Glassmorphism)`
+const GlassmorphismStyled = styled(TransparentGlassOrDarkBlurView)`
   border-radius: ${(props) => props.theme.radii.md}px;
   margin-right: auto;
   margin-bottom: ${(props) => props.theme.spacing[3]}px;
@@ -18,7 +18,8 @@ const GlassmorphismStyled = styled(Glassmorphism)`
 `;
 
 const ViewStyled = styled(View)`
-  padding: ${(props) => props.theme.spacing[2]}px ${(props) => props.theme.spacing[4]}px;
+  padding: ${(props) => props.theme.spacing[2]}px
+    ${(props) => props.theme.spacing[4]}px;
   padding-bottom: ${(props) => props.theme.spacing[2.5]}px;
 `;
 
